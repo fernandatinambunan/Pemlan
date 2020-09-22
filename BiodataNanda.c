2aaf70df5745a647f0de87a64269bc6814e8e141
fernandatinambunan <nandatinambunan13@gmail.com>
2020-09-22 11:22:51 +0700

    first commit

---
+++ BiodataNanda.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+printf("Nama \t\t: Fernanda Tinambunan\n");
+printf("TTD  \t\t: Sidoarjo, 11 Oktober 2000\n");
+printf("Alamat \t\t: Jl. S. Parman V Waru\n");
+printf("Jenis Kelamin \t: Perempuan\n");
+printf("NPM \t\t: 19081010179\n");
+printf("Kewarganegaraan : Indonesia\n");
+printf("Email \t\t: nandatinambunan13@gmail.com\n");
+}
